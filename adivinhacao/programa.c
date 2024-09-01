a1693382c96f9936cbbefc02fa6ad723951f5000
Bowie <rafaeldiassilv@gmail.com>
2024-09-01 19:31:50 -0300

    estudos em C

--- adivinhacao/programa.c
+++ adivinhacao/programa.c
@@ -1,43 +1,60 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main(){
+#define NUMERO_TENTATIVAS 5
+
+int main(void)
+{
+
     setlocale(LC_ALL, "Portuguese");
 
-    //imprime o cabeçalho do Jogo!
+    // imprime o cabeçalho do Jogo!
     printf("*******************************************\n");
     printf("* Bem vindo ao nosso Jogo de Adivinhação! *\n");
     printf("*******************************************\n");
 
-    // declarando uma variavel do tipo inteiro e iniciando ela
+    // for (int i = 1; i <= NUMERO_TENTATIVAS; i++)
     int numeroSecreto, chute;
-    numeroSecreto = 42;
+    int i = 1;
+    while (1)
+    {
+        // declarando uma variavel do tipo inteiro e iniciando ela
+        numeroSecreto = 42;
 
-    printf("Qual é o seu chute? \n");
+        printf("tentativa %d\n", i);
 
-    // scanf lê do teclado 
-    // %d Mascara da Variavel
-    // operador "&" (endereço de) no caso do scanf retorna o endereço na memória onde a variavel está armazenada
-    scanf("%d", &chute);
+        printf("Qual é o seu chute? \n");
 
-    printf("O seu chute foi %d\n", chute);
+        scanf("%d", &chute); // operador "&" (endereço de) no caso do scanf retorna o endereço na memória
+        printf("O seu chute foi %d\n", chute);
 
-    int acertou = (chute == numeroSecreto);
-
-    if (acertou){
-        printf("Prabéns o Numero Secreto é %d e você Acertou\n", numeroSecreto);
-
-    } else {
-
+        int acertou = chute == numeroSecreto;
         int maior = chute > numeroSecreto;
-        if(maior){
-            printf("O chute é maior que o Numero Secreto!!!\n");
-        } else {
-            printf("o chute é menor que o numero secreto!!!\n");
+
+        if (chute < 0)
+        {
+            printf("Você não pode chutar numero é negativo\n");
+            continue;
         }
 
-        printf("você errou\n");
-        printf("Tente novamente\n");
+        if (acertou)
+        {
+            printf("Prabéns o Numero Secreto é %d e você Acertou\n", numeroSecreto);
+            break;
+        }
+        else if (maior)
+        {
+            printf("o chute é maior que o numero secreto!!!\n");
+            printf("você errou\n");
+            printf("Tente novamente\n");
+        }
+        else
+        {
+            printf("O chute é menor que o Numero Secreto!!!\n");
+            printf("você errou\n");
+            printf("Tente novamente\n");
+        }
+        i++;
     }
-
+    printf("Fim de Jogo.");
 }
